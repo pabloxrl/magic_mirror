6f6ac8a63c1b478317e92f347821718a955305ae
unknown <plorenzo@SEABERRY.NewnhamResearch.local>
2015-10-20 15:37:53 +0200

    Added weather and beginning of css

--- js/scheduler.js
+++ js/scheduler.js
@@ -1,5 +1,3 @@
-var clockTimeout = 1000;
-
 function updateClock() {
   
   this.needsUpdate = function (now) {
@@ -20,12 +18,13 @@ function updateClock() {
   
   var currentTime = new Date();
   
-  if (needsUpdate(currentTime.getMinutes())) {
+  if (this.needsUpdate(currentTime.getMinutes())) {
+    
     if (document.getElementById('clock').hasChildNodes()) {
-      document.getElementById('clock').childNodes[0].innerHTML = formatOutput(currentTime.getHours(), currentTime.getMinutes());
+      document.getElementById('clock').childNodes[0].innerHTML = this.formatOutput(currentTime.getHours(), currentTime.getMinutes());
     } else {
       var timer = document.createElement('div');
-      timer.innerHTML = formatOutput(currentTime.getHours(), currentTime.getMinutes()); 
+      timer.innerHTML = this.formatOutput(currentTime.getHours(), currentTime.getMinutes()); 
       document.getElementById('clock').appendChild(timer);
     }
   
@@ -69,13 +68,13 @@ function updateCalendar() {
   
   var currentTime = new Date();
   
-  if (needsUpdate(currentTime.getDay())) {
+  if (this.needsUpdate(currentTime.getDay())) {
     
     if (document.getElementById('calendar').hasChildNodes()) {
       document.getElementById('calendar').childNodes[0].innerHTML = formatOutput(currentTime.getDay(), currentTime.getDate(), currentTime.getMonth(), currentTime.getFullYear());
     } else {
       var e = document.createElement('div');
-      e.innerHTML = formatOutput(currentTime.getDay(), currentTime.getDate(), currentTime.getMonth(), currentTime.getFullYear());
+      e.innerHTML = this.formatOutput(currentTime.getDay(), currentTime.getDate(), currentTime.getMonth(), currentTime.getFullYear());
       document.getElementById('calendar').appendChild(e);
     }
     
@@ -85,11 +84,46 @@ function updateCalendar() {
     console.log("No need to update calendar");
     return;
   }
+}
+
+function updateWeather() {
   
+  this.updateNode = function (id, information) {
+    
+    var root = document.getElementById(id);
+    
+    if (!root.hasChildNodes()) {
+      var e = document.createElement('div');
+      root.appendChild(e);
+    }
+    
+    root.childNodes[0].innerHTML = information;
+  }
+  
+  this.formatOutput = function (data) {
+    
+    this.updateNode('city', data.name);
+    this.updateNode('icon', data.weather[0].icon);
+    this.updateNode('temperature', Math.round(data.main.temp) + '°C');
+    this.updateNode('summary', data.weather[0].main);
+    this.updateNode('maxmin_temp', 'min: ' + Math.round(data.main.temp_min) + '°C / max: ' + Math.round(data.main.temp_max) + '°C');
+  }
+
+  $.ajax({
+    dataType    : "jsonp",
+    url         : 'http://api.openweathermap.org/data/2.5/weather?q=Tychy&units=metric&APPID=9e19774d951adb24a6e02b9b8b48d9df',
+    success     : function (data) { formatOutput(data); }
+  });
 }
 
 function launchSchedules() {
   
-  setInterval(updateClock, clockTimeout);
-  setInterval(updateCalendar, clockTimeout);
+  var ONE_SECOND_TIMEOUT = 1000;
+  var ONE_HOUR_TIMEOUT = 60 * 60 * ONE_SECOND_TIMEOUT;
+  
+  setInterval(updateClock, ONE_SECOND_TIMEOUT);
+  setInterval(updateCalendar, ONE_SECOND_TIMEOUT);
+  
+  updateWeather();
+  setInterval(updateWeather, ONE_HOUR_TIMEOUT);
 }
